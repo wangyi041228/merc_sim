018269e46f5bba54863a922e6f7d777fd28e98b1
E <18771051812@qq.com>
2021-09-04 23:49:00 +0800

    210904

--- merc_sim.py
+++ merc_sim.py
@@ -4,6 +4,7 @@ from enum import Enum
 from PIL import Image, ImageTk  # ImageGrab, ImageChops
 import os
 import ctypes
+import random
 
 g_s = ctypes.windll.user32.GetSystemMetrics
 screensize = g_s(78), g_s(79)
@@ -25,23 +26,84 @@ MERC_L = {
 MERC_L_R = dict(zip(MERC_L.values(), MERC_L.keys()))
 MERC_L_CB = tuple(MERC_L.values())
 
-RACE = Enum('RACE', ('None', 'Tauren', 'Human', 'Orc', 'Beast', 'Demon', 'Murloc', 'Element', 'Night Elf', 'Gnome', 'Undead',
-                     'Dwarf', 'Blood Elf', 'Troll', 'High Elf', 'Dragon', 'Draenei'))
-M_TYPE = Enum('M_TYPE', 'Protector Fighter Caster')
+RACE = Enum('RACE',
+            ('None', 'Tauren', 'Human', 'Orc', 'Beast', 'Demon', 'Murloc', 'Element', 'Night Elf', 'Gnome', 'Undead',
+             'Dwarf', 'Blood Elf', 'Troll', 'High Elf', 'Dragon', 'Draenei'))
+RACE_L = {
+    RACE['None']: '无种族',
+    RACE['Tauren']: '牛头人',
+    RACE['Human']: '人类',
+    RACE['Orc']: '兽人',
+    RACE['Beast']: '野兽',
+    RACE['Demon']: '恶魔',
+    RACE['Murloc']: '鱼人',
+    RACE['Element']: '元素',
+    RACE['Night Elf']: '暗夜精灵',
+    RACE['Gnome']: '侏儒',
+    RACE['Undead']: '亡灵',
+    RACE['Dwarf']: '矮人',
+    RACE['Blood Elf']: '血精灵',
+    RACE['Troll']: '巨魔',
+    RACE['High Elf']: '高等精灵',
+    RACE['Dragon']: '龙',
+    RACE['Draenei']: '德莱尼',
+}
+M_TYPE = Enum('M_TYPE', ('None', 'Protector', 'Fighter', 'Caster'))
+M_TYPE_L = {
+    M_TYPE['None']: '无业',
+    M_TYPE['Protector']: '护卫',
+    M_TYPE['Fighter']: '斗士',
+    M_TYPE['Caster']: '施法者',
+}
+S_TYPE = Enum('S_TYPE', ('None', 'Holy', 'Arcane'))
+S_TYPE_L = {
+    S_TYPE['None']: '无',
+    S_TYPE['Holy']: '神圣',
+    S_TYPE['Arcane']: '奥术',
+}
 SKILL = Enum('SKILL', (
     'None',
+    # Cariel Roame
     "Crusader's Blow",
+    'Taunt',
+    'Seal of Light',
+    # Tyrande
     'Arcane Shot',
+    'Arcane Salvo',
+    "Elune's Grace",
+    # Xyrella
     'Blinding Luminance',
+    'Flash Heal',
+    'Atonement',
 ))
 SKILL_L = {
-    SKILL['None']: '留空',
+    SKILL['None']: '休息',
     SKILL["Crusader's Blow"]: '远征军打击',
-    SKILL['Arcane Shot']: '奥术齐射',
+    SKILL['Taunt']: '嘲讽',
+    SKILL['Seal of Light']: '光明圣印',
+    SKILL['Arcane Shot']: '奥术射击',
+    SKILL['Arcane Salvo']: '奥术齐射',
+    SKILL["Elune's Grace"]: '艾露恩的赐福',
     SKILL['Blinding Luminance']: '致盲之光',
+    SKILL['Flash Heal']: '快速治疗',
+    SKILL['Atonement']: '救赎',
 }
 SKILL_L_R = dict(zip(SKILL_L.values(), SKILL_L.keys()))
 SKILL_L_CB = tuple(SKILL_L.values())
+
+SKILL_D = {
+    SKILL['None']: [S_TYPE['None']] + [(114, '为什么佣兵战纪不能休息？')] * 5,
+    SKILL["Crusader's Blow"]: [S_TYPE['Holy']] + [(6, '攻击一个敌人。击杀：为此佣兵恢复10点生命值。')] * 5,
+    SKILL['Taunt']: [S_TYPE['None']] + [(1, '为此佣兵恢复4点生命值，并使其在本回合中获得嘲讽。')] * 5,
+    SKILL['Seal of Light']: [S_TYPE['Holy']] + [(4, '选择一个角色，使其获得+2攻击力并为其恢复5点生命值。')] * 5,
+    SKILL['Arcane Shot']: [S_TYPE['Arcane']] + [(7, '对一个敌人造成4点伤害。')] * 5,
+    SKILL['Arcane Salvo']: [S_TYPE['Arcane']] + [(5, '随机对两个敌人造成2点伤害。')] * 5,
+    SKILL["Elune's Grace"]: [S_TYPE['Arcane']] + [(6, '你的队伍的下一个奥术技能会施放两次，且速度值永久加快（1）点。')] * 5,
+    SKILL['Blinding Luminance']: [S_TYPE['Holy']] + [(3, '对一个敌人造成2点伤害，并使其在本回合获得-2攻击力。')] * 5,
+    SKILL['Flash Heal']: [S_TYPE['Holy']] + [(4, '恢复5点生命值。')] * 5,
+    SKILL['Atonement']: [S_TYPE['Holy']] + [(9, '造成9点伤害。你的队伍每恢复20点生命值，伤害+1。')] * 5,
+}
+
 SKILL_LEVEL_CB = (1, 2, 3, 4, 5)
 SKILL_TARGET_CB = (0, 1, 2, 3, 4, 5, 6, 7, -1, -2, -3, -4, -5, -6, -7)
 
@@ -52,14 +114,14 @@ ITEM = Enum('ITEM', (
 ))
 
 D = {
-    MERC['None']: (M_TYPE['Protector'], RACE['None'], (), (), (
+    MERC['None']: (M_TYPE['None'], RACE['None'], (SKILL['None'],), (), (
         (0, 0), (0, 0),
     )),
     MERC['Cariel Roame']: (M_TYPE['Protector'], RACE['Human'], (SKILL["Crusader's Blow"],), (ITEM['A'],), (
         (3, 10), (3, 10),
     )),
     MERC['Tyrande']: (M_TYPE['Fighter'], RACE['Night Elf'], (SKILL['Arcane Shot'],), (ITEM['B'],), (
-        (2, 11), (2, 11),
+        (1, 9), (1, 9),
     )),
     MERC['Xyrella']: (M_TYPE['Caster'], RACE['Draenei'], (SKILL['Blinding Luminance'],), (ITEM['C'],), (
         (1, 7), (1, 7),
@@ -69,14 +131,14 @@ D = {
     ))}
 L = 1  # 初始等级
 params = {
-    'w': 690,
-    'h': 650,
+    'w': 685,
+    'h': 655,
     'x': 50,
     'y': 50,
     't': 1,
-    'e': [(MERC[n], L, D[MERC[n]][4][L][0], D[MERC[n]][4][L][1], (), (), ()) for n in (
+    'e': [(MERC[n], L, D[MERC[n]][4][L][0], D[MERC[n]][4][L][1], (SKILL['None'], 1, 0), (), ()) for n in (
         'Cariel Roame', 'Tyrande', 'Xyrella')],
-    'a': [(MERC[n], L, D[MERC[n]][4][L][0], D[MERC[n]][4][L][1], (), (), ()) for n in (
+    'a': [(MERC[n], L, D[MERC[n]][4][L][0], D[MERC[n]][4][L][1], (SKILL['None'], 1, 0), (), ()) for n in (
         'Cariel Roame', 'Tyrande', 'Xyrella')]
 }
 
@@ -85,6 +147,7 @@ class MainWindow(Tk):
     class SideWindow(Toplevel):
         def __init__(self):
             super().__init__()
+            self.parent = self.nametowidget(self.winfo_parent())
             self.t = params['t']
             self.title(f'第{str(self.t)}回合')
             self.w = params['w']
@@ -100,16 +163,19 @@ class MainWindow(Tk):
             self.update_bottum.pack()
             self.update_bottum.place(x=150, y=310)
 
+            self._roll_entry = Entry(self, width=3)
+            self._roll_entry.insert(0, '2')
+            self._roll_entry.pack()
+            self._roll_entry.place(x=400, y=310)
+
+            self.roll_bottum = Button(self, text='掷骰', width=10, command=self.roll)
+            self.roll_bottum.pack()
+            self.roll_bottum.place(x=300, y=310)
+
             self.e = params['e']
             self.ef = []
             self.a = params['a']
             self.af = []
-
-            # 结算技能，更新数值
-            this_log = '\n新一回合'
-            parent = self.nametowidget(self.winfo_parent())
-            parent.log_label.insert(END, this_log)
-            parent.log_label.see(END)
 
             i = 0
             for merc in self.e:
@@ -125,35 +191,35 @@ class MainWindow(Tk):
                 _item = merc[5]
                 _passive = merc[6]
 
-                _lable_frame = LabelFrame(self, text=MERC_L[merc[0]], height=300, width=220)
+                _lable_frame = LabelFrame(self, text=MERC_L[merc[0]], height=310, width=220)
                 _lable_frame.pack()
                 _lable_frame.place(x=i * 230, y=0)
 
                 _image_lable = Label(_lable_frame)
                 _image_lable.pack()
-                _image_lable.place(x=0, y=100)
+                _image_lable.place(x=0, y=110)
                 self.update_merc_image(_image_lable, _merc_name)
 
-                _merc_combobox = Combobox(_lable_frame)
+                _merc_combobox = Combobox(_lable_frame, width=18)
                 _merc_combobox['values'] = MERC_L_CB
                 _merc_combobox.set(MERC_L[_merc_name])
                 _merc_combobox.pack()
-                _merc_combobox.place(x=0, y=200)
+                _merc_combobox.place(x=2, y=210)
 
                 _level_entry = Entry(_lable_frame, width=3)
                 _level_entry.insert(0, str(merc[1]))
                 _level_entry.pack()
-                _level_entry.place(x=60, y=255)
+                _level_entry.place(x=60, y=265)
 
                 _atk_entry = Entry(_lable_frame, width=4)
                 _atk_entry.insert(0, str(merc[2]))
                 _atk_entry.pack()
-                _atk_entry.place(x=20, y=230)
+                _atk_entry.place(x=20, y=240)
 
                 _hp_entry = Entry(_lable_frame, width=4)
                 _hp_entry.insert(0, str(merc[3]))
                 _hp_entry.pack()
-                _hp_entry.place(x=105, y=230)
+                _hp_entry.place(x=105, y=240)
 
                 _skill_combobox = Combobox(_lable_frame)
                 _skill_combobox['values'] = SKILL_L_CB
@@ -196,7 +262,7 @@ class MainWindow(Tk):
                 _item = merc[5]
                 _passive = merc[6]
 
-                _lable_frame = LabelFrame(self, text=MERC_L[merc[0]], height=300, width=220)
+                _lable_frame = LabelFrame(self, text=MERC_L[merc[0]], height=310, width=220)
                 _lable_frame.pack()
                 _lable_frame.place(x=i * 230, y=340)
 
@@ -205,11 +271,11 @@ class MainWindow(Tk):
                 _image_lable.place(x=0, y=0)
                 self.update_merc_image(_image_lable, _merc_name)
 
-                _merc_combobox = Combobox(_lable_frame)
+                _merc_combobox = Combobox(_lable_frame, width=18)
                 _merc_combobox['values'] = MERC_L_CB
                 _merc_combobox.set(MERC_L[_merc_name])
                 _merc_combobox.pack()
-                _merc_combobox.place(x=0, y=100)
+                _merc_combobox.place(x=2, y=100)
 
                 _level_entry = Entry(_lable_frame, width=3)
                 _level_entry.insert(0, str(merc[1]))
@@ -253,7 +319,48 @@ class MainWindow(Tk):
                                 _skill_combobox, _skill_level_combobox, _skill_target_combobox, _skill_image_lable))
                 i += 1
 
+            # 结算技能 动画 输出日志
+            self.log('\n————————\n\n开战！\n\n【敌方】\n')
+
+            for e in self.ef:
+                _d = e[2].get(), e[3].get(), e[4].get(), e[5].get(), e[6].get(), e[7].get(), e[8].get()
+                _m = MERC_L_R[_d[0]]
+                if _m == [MERC['None']]:
+                    continue
+                _log = f'({_d[1]}级 {RACE_L[D[_m][1]]} {M_TYPE_L[D[_m][0]]}) {MERC_L[_m]} 属性：{_d[2]}/{_d[3]}\n'
+                self.log(_log)
+                _skill = SKILL_L_R[_d[4]]
+                if _skill != SKILL['None']:
+                    _t = SKILL_D[_skill][0]
+                    _s, _line = SKILL_D[_skill][int(_d[5])]
+                    _p = f'对【{_d[6]}】号位' if _d[6] != '0' else ''
+                    _log = f'　　{_p}预备【{_d[4]}{_d[5]}】。\n　　{str(_s)}速，{S_TYPE_L[_t]}属性，{_line}\n'
+                    self.log(_log)
+
+            self.log('\n【己方】\n')
+
+            for e in self.af:
+                _d = e[2].get(), e[3].get(), e[4].get(), e[5].get(), e[6].get(), e[7].get(), e[8].get()
+                _m = MERC_L_R[_d[0]]
+                if _m == [MERC['None']]:
+                    continue
+                _log = f'({_d[1]}级 {RACE_L[D[_m][1]]} {M_TYPE_L[D[_m][0]]}){MERC_L[_m]} 属性：{_d[2]}/{_d[3]}\n'
+                self.log(_log)
+                _skill = SKILL_L_R[_d[4]]
+                if _skill != SKILL['None']:
+                    _t = SKILL_D[_skill][0]
+                    _s, _line = SKILL_D[_skill][int(_d[5])]
+                    _p = f'对【{_d[6]}】号位' if _d[6] != '0' else ''
+                    _log = f'　　{_p}预备【{_d[4]}{_d[5]}】。\n　　{str(_s)}速，{S_TYPE_L[_t]}属性，{_line}\n'
+                    self.log(_log)
+
+            self.log(f'\n手动结算，之后开始第{str(self.t)}回合。\n')
+
             self.mainloop()
+
+        def log(self, logs):
+            self.parent.log_label.insert(END, logs)
+            self.parent.log_label.see(END)
 
         def update_merc_image(self, _lable: Widget, _merc_name):
             _p = os.path.join('.', 'merc', str(_merc_name.value) + '.png')
@@ -293,7 +400,7 @@ class MainWindow(Tk):
             params = {
                 'w': self.winfo_width(),
                 'h': self.winfo_height(),
-                'x': min(self.winfo_x() + 100, screensize[0] - self.winfo_width()),  # 忽略多屏DPI不同的问题
+                'x': min(self.winfo_x() + self.winfo_width(), screensize[0] - self.winfo_width()),  # 忽略多屏DPI不同的问题
                 'y': self.winfo_y(),
                 't': self.t + 1,
                 'e': new_e,
@@ -305,6 +412,11 @@ class MainWindow(Tk):
 
         def get_f2(self, af_ef):
             return [(e[2].get(), e[3].get(), e[4].get(), e[5].get(), e[6].get(), e[7].get(), e[8].get()) for e in af_ef]
+
+        def roll(self):
+            num = self._roll_entry.get()
+            _ro = random.randint(1, int(num))
+            self.log(f'1到{num}随机数：{str(_ro)}\n')
 
         def _update(self):
             for af_ef in (self.af, self.ef):
@@ -329,18 +441,18 @@ class MainWindow(Tk):
 
     def __init__(self):
         super().__init__()
-        self.title('佣兵模拟器')
+        self.title('佣兵沙盘')
         # 主窗口放使用教程
-        self.w = 250
-        self.h = 200
-        self.x = 50
-        self.y = 800
+        self.w = 600
+        self.h = 400
+        self.x = 80
+        self.y = 80
         self.geometry(f'{str(self.w)}x{str(self.h)}+{str(self.x)}+{str(self.y)}')
-        self.log = StringVar(value='欢迎使用佣兵模拟器DEMO。数值不完整，某些操作会引发错误。'
-                                   '切技能换目标不需要点更新英雄。更新英雄会同刷新图片。')
+        self.log = StringVar(value='欢迎使用佣兵沙盘DEMO。\n数值不完整，某些操作会报错。\n'
+                                   '切技能换目标不需要点更新英雄。\n更新英雄会同刷新图片。未来可能补自动结算。\n日志可擦除。\n')
         self.log_label = Text(self)
         self.log_label.insert(END, self.log.get())
-        self.log_label.pack(fill=BOTH)
+        self.log_label.pack(expand=1, fill=BOTH)
 
         self.SideWindow()
 
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 from enum import Enum
 RACE = Enum('RACE', ('Tauren', 'Human', 'Orc', 'Beast', 'Demon', 'Murloc', 'Element', 'Night Elf', 'Gnome', 'Undead',
                      'Dwarf', 'Blood Elf', 'Troll', 'High Elf', 'Dragon'))
@@ -10,3 +12,11 @@ for name, member in M_TYPE.__members__.items():
 
 for a, b in ((1, 2), (3, 4)):
     print(a, b)
+
+x = [(114, '休息了一下。')] * 5
+print(x)
+a = [] + x
+print(a)
+
+for _ in range(20):
+    print(random.randint(1, 10))
